24df4a97d0645ead8420e20872964a91491fae9c
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 17:18:35 +0700

    Add reviewed Science G3 Video 44 lesson

---
+++ 7ddd0f64383c/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-44",
+  "number": 44,
+  "title": "Technology",
+  "duration": 409.0195,
+  "durationLabel": "6 phút 49 giây",
+  "total": 30,
+  "audio": "7ddd0f64383c/audio.mp3",
+  "mindmap": "7ddd0f64383c/mindmap.png",
+  "questionsImage": "7ddd0f64383c/questions.jpg",
+  "speakingPoints": [
+    "Knowledge into tools",
+    "Plumbing systems",
+    "Heating and cooling",
+    "Building technology",
+    "Kitchen technology",
+    "Recording music"
+  ],
+  "questions": [
+    {
+      "text": "What is technology? Explain why both a sewing needle and a computer can be examples of technology."
+    },
+    {
+      "text": "How do parts of a plumbing system work together? Explain how a water heater changes the job of preparing a warm bath."
+    },
+    {
+      "text": "Compare heating and cooling systems. What problem does each solve, and what do they have in common?"
+    },
+    {
+      "text": "Choose two kitchen technologies from the video. Explain how each helps people prepare or preserve food."
+    },
+    {
+      "text": "How did recording technology change the way people share music? Compare live performance with playing a recording."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "What technology means",
+      "start": 12.85,
+      "end": 62.39,
+      "timeLabel": "0:12–1:02",
+      "parts": [
+        "Today we're going to be studying technology. Technology affects our lives in many different ways. And when we study science, ",
+        {
+          "id": 1,
+          "answer": "we learn how the world works"
+        },
+        ". And technology helps us to take this understanding and ",
+        {
+          "id": 2,
+          "answer": "use it to solve problems"
+        },
+        ", to make life better. Technology is the use of knowledge to design new tools and new ways to do things. So we take the knowledge that we learn about science and then when we use it to do something, then we have technology. A tool can be ",
+        {
+          "id": 3,
+          "answer": "as simple as a sewing needle"
+        },
+        " or as complicated as a computer. Now, technology over the years has progressed a lot and people have used things that they've learned to make different tools. "
+      ]
+    },
+    {
+      "title": "Invention takes effort",
+      "start": 62.78,
+      "end": 103.09,
+      "timeLabel": "1:02–1:43",
+      "parts": [
+        "An invention is ",
+        {
+          "id": 4,
+          "answer": "something made for the first time"
+        },
+        ". Now, technology has come a long ways because people have made inventions. They've invented things. But when people invent things, it can take a lot of work. Take, for example, the famous inventor Thomas Edison. He invented the light bulb, but it took a lot of work for him to do this. He had to be ",
+        {
+          "id": 5,
+          "answer": "very persistent and not give up"
+        },
+        ". And so when people invent things, ",
+        {
+          "id": 6,
+          "answer": "it often takes a lot of effort"
+        },
+        ". But because they've made that effort, our lives are so different today. We have light bulbs because Thomas Edison had an idea and he didn't give up. "
+      ]
+    },
+    {
+      "title": "Systems in the home",
+      "start": 103.42999999999999,
+      "end": 139.09,
+      "timeLabel": "1:43–2:19",
+      "parts": [
+        "Let's talk about some different ",
+        {
+          "id": 7,
+          "answer": "types of technology that we use every single day"
+        },
+        ". Technology in the home. ",
+        {
+          "id": 8,
+          "answer": "Our homes are filled with technology"
+        },
+        ". And there are actually ",
+        {
+          "id": 9,
+          "answer": "many different systems of technology that work together"
+        },
+        ". The plumbing system. The plumbing system is made of parts like pipes, bathtubs, sinks, toilets, drains, faucets, has many different parts and they work together. They're where we use the water in our house. "
+      ]
+    },
+    {
+      "title": "Water and plumbing",
+      "start": 139.79999999999998,
+      "end": 187.53,
+      "timeLabel": "2:19–3:07",
+      "parts": [
+        "And some of this water also ",
+        {
+          "id": 10,
+          "answer": "goes through a water heating unit"
+        },
+        ". And the water heater heats up the water so that we have warm water and we turn on the hot water. And if we didn't have this technology, imagine trying to take a hot shower. It wouldn't be very fun because we'd only have cold water. But because we have the technology of the plumbing system, and part of it is that water heater, then we have ",
+        {
+          "id": 11,
+          "answer": "hot water when we turn on the faucet"
+        },
+        ". A long time ago, if they wanted to take a hot bath, they had to ",
+        {
+          "id": 12,
+          "answer": "find a different way to heat their water"
+        },
+        ". They didn't have electricity readily available, so they had to heat their water on a stove. or somehow and then dump it into the tub and it was a huge, huge job just to take a hot bath. Today it's something we kind of take for granted. "
+      ]
+    },
+    {
+      "title": "Cooling indoor air",
+      "start": 188.26,
+      "end": 237.94,
+      "timeLabel": "3:08–3:57",
+      "parts": [
+        "",
+        {
+          "id": 13,
+          "answer": "The heating and cooling system"
+        },
+        ". Now this system has to do with the air conditioning and the furnace that heats up the air in the winter. And this system has different parts. The air conditioning unit that's outside and then there's also the furnace. And then there's vents, pipes and vents that the air comes out in so it's ",
+        {
+          "id": 14,
+          "answer": "the right temperature throughout our house"
+        },
+        ". During hot hot summer days, we turn on the air conditioning and ",
+        {
+          "id": 15,
+          "answer": "it keeps the air in the house cool"
+        },
+        " and a nice comfortable temperature. People didn't used to always have air conditioning. In fact, it was invented by a doctor who was trying to cool down the rooms for his patients with fevers. And now, air conditioning has come a long ways, and we can put it right in our houses. "
+      ]
+    },
+    {
+      "title": "Heating indoor air",
+      "start": 238.92,
+      "end": 264.07,
+      "timeLabel": "3:58–4:24",
+      "parts": [
+        "And then on the winter days, ",
+        {
+          "id": 16,
+          "answer": "when it's very, very cold outside"
+        },
+        ", we can turn on the furnace, and ",
+        {
+          "id": 17,
+          "answer": "the furnace will heat up the air"
+        },
+        ". And we just need to turn on the furnace that uses electricity. A long time ago, they had to use things like wood and a fire. They'd have to make sure there was enough dry, chopped wood, and they'd burn it in the fire ",
+        {
+          "id": 18,
+          "answer": "to make warmth for their house"
+        },
+        ". Now it's so easy. we can just turn on the warm air. "
+      ]
+    },
+    {
+      "title": "Technology in construction",
+      "start": 264.81,
+      "end": 294.74,
+      "timeLabel": "4:24–4:54",
+      "parts": [
+        "",
+        {
+          "id": 19,
+          "answer": "Another place we use technology is in construction"
+        },
+        ". Now construction is when buildings are built and the construction of buildings has come a long ways. People use technology to ",
+        {
+          "id": 20,
+          "answer": "make the buildings sturdy"
+        },
+        " and also we use a lot of technology in the materials that we use to build the buildings. We have insulation that helps keep cold drafts from going into the buildings and many different building materials that use technology. so ",
+        {
+          "id": 21,
+          "answer": "the buildings are sturdy and comfortable"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Technology in the kitchen",
+      "start": 295.43,
+      "end": 341.53000000000003,
+      "timeLabel": "4:55–5:41",
+      "parts": [
+        "Now we also have a lot of technology in our kitchens. ",
+        {
+          "id": 22,
+          "answer": "We have an electric refrigerator and freezer"
+        },
+        ". We have an electric stove and oven. We have an electric microwave oven. There's so many different things in our kitchen that use electricity. Well, a long time ago, this ",
+        {
+          "id": 23,
+          "answer": "electricity wasn't so readily available"
+        },
+        ". If people wanted to preserve their food, it wasn't so easy. and then as technology evolved then people started making ice boxes that had big blocks of ice and they could use those ice boxes to keep their food cold so it stayed good longer and then we came out with the electric refrigerators and freezers and now it's ",
+        {
+          "id": 24,
+          "answer": "a lot easier to preserve our food"
+        },
+        " and it's a lot easier to cook food too. "
+      ]
+    },
+    {
+      "title": "Cooking and recorded music",
+      "start": 341.68,
+      "end": 389.32,
+      "timeLabel": "5:41–6:29",
+      "parts": [
+        "A long time ago people had to cut the wood for the stove they had to do all these different things to prepare the food. It could take almost a whole day just to make one meal. Now We can ",
+        {
+          "id": 25,
+          "answer": "make food a lot faster"
+        },
+        " using the new technology that we have. And music has also come a long way in technology. It used to be if you want to share music you had to get someone to sing or play an instrument because we didn't have a way to ",
+        {
+          "id": 26,
+          "answer": "record and play back the music"
+        },
+        ". Then different things were invented and people were able to record the music, and we could play it back. We started using records, and then we started using CDs, and now we can play music from just ",
+        {
+          "id": 27,
+          "answer": "using a little MP3 player"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Ideas for the future",
+      "start": 389.99,
+      "end": 406.44,
+      "timeLabel": "6:29–6:46",
+      "parts": [
+        "And ",
+        {
+          "id": 28,
+          "answer": "technology continues to evolve"
+        },
+        ". So technology is amazing, and ",
+        {
+          "id": 29,
+          "answer": "we use it every single day"
+        },
+        ". Sometimes it's really fun to think about how much more technology will have in the future. And who knows? Maybe you will be ",
+        {
+          "id": 30,
+          "answer": "one of the new inventors of the future"
+        },
+        ". "
+      ]
+    }
+  ]
+};
