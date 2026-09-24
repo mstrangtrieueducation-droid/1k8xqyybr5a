c0277af5af581776d5616c125e24ad95619de30a
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 16:39:51 +0700

    Add reviewed Science G3 Video 40 lesson

---
+++ db63b9cee9b6/lesson.js
@@ -0,0 +1,260 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-40",
+  "number": 40,
+  "title": "Pulley",
+  "duration": 364.5300625,
+  "durationLabel": "6 phút 4 giây",
+  "total": 30,
+  "audio": "db63b9cee9b6/audio.mp3",
+  "mindmap": "db63b9cee9b6/mindmap.png",
+  "questionsImage": "db63b9cee9b6/questions.jpg",
+  "speakingPoints": [
+    "How a pulley is built",
+    "A flagpole",
+    "Sails on a boat",
+    "Change pull direction",
+    "A pulley system",
+    "Less pulling force"
+  ],
+  "questions": [
+    {
+      "text": "What is a pulley, and what does the groove do? Identify the rope, wheel, and load in the lesson's demonstration."
+    },
+    {
+      "text": "How do pulleys help raise and lower a flag? Explain why the person does not need to climb the flagpole."
+    },
+    {
+      "text": "How do sailors use pulleys to position sails or lift a fishing net? Choose one example and explain the rope's role."
+    },
+    {
+      "text": "In the single overhead pulley demonstration, which way does the teacher pull and which way does the jug move? What does the pulley change?"
+    },
+    {
+      "text": "Why is lifting the jug easier with the demonstrated pulley system? Does the jug become lighter, and what must happen to the length of rope pulled?"
+    }
+  ],
+  "chunks": [
+    {
+      "title": "The wheel, groove, and rope",
+      "start": 13.139999999999999,
+      "end": 53.97,
+      "timeLabel": "0:13–0:53",
+      "parts": [
+        "Today we're going to be talking about pulleys. Now a pulley is ",
+        {
+          "id": 1,
+          "answer": "another simple machine that helps to make work easier"
+        },
+        ". Remember work is ",
+        {
+          "id": 2,
+          "answer": "when we use force to move an object"
+        },
+        ". So a pulley is a simple machine that ",
+        {
+          "id": 3,
+          "answer": "uses grooved wheels and a rope"
+        },
+        " to raise, lower, or move a load. So if you look at this pulley that I brought today, you can see it's a wheel. And on this wheel, There's a groove. See the groove in there? There's raised sides. These raised sides help so that ",
+        {
+          "id": 4,
+          "answer": "the rope doesn't slip off"
+        },
+        " while we're using the pulley. "
+      ]
+    },
+    {
+      "title": "Raise and lower a flag",
+      "start": 55.08,
+      "end": 117.62,
+      "timeLabel": "0:55–1:57",
+      "parts": [
+        "Now, what are some things that we use pulleys for? Well, one thing that we can use a pulley for is with a flagpole. ",
+        {
+          "id": 5,
+          "answer": "We use pulleys to help raise the flag"
+        },
+        " to the top of the flagpole. Now, if you imagine trying to get the flag to the top of a flagpole, ",
+        {
+          "id": 6,
+          "answer": "if we had to climb the flagpole"
+        },
+        ", hang the flag up there at the top, and then climb down, that would be really difficult. Not to mention, dangerous. But if we use pulleys, we can have a pulley attached on here, and at the top of the flagpole, and then a rope going around the pulleys. And so to raise the flag, all we need to do is ",
+        {
+          "id": 7,
+          "answer": "connect the flag to that rope"
+        },
+        " and pull the rope up, secure it, and the flag is up at the top of the flagpole. When we want to take it down, then we can unsecure it, pull the other side of the rope down, and down the flag comes ",
+        {
+          "id": 8,
+          "answer": "so we can take it off and put it away"
+        },
+        ". So pulleys help us a lot in raising flags on the flagpole. "
+      ]
+    },
+    {
+      "title": "Pulleys on boats",
+      "start": 120.28,
+      "end": 150.42000000000002,
+      "timeLabel": "2:00–2:30",
+      "parts": [
+        "",
+        {
+          "id": 9,
+          "answer": "Pulleys are also used on boats"
+        },
+        ". Pulleys help to position sails just right so they can ",
+        {
+          "id": 10,
+          "answer": "catch the wind to make the boat move"
+        },
+        ". Here you can see some pulleys on the boat and they're attached to the sails. So ",
+        {
+          "id": 11,
+          "answer": "sailors can pull the ropes"
+        },
+        " and the pulleys help to position the sails just right so that they can catch the wind. And when these sailboats have their sails catch the wind, then ",
+        {
+          "id": 12,
+          "answer": "those boats can move across the water"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Lift a fishing net",
+      "start": 153.6,
+      "end": 190.62,
+      "timeLabel": "2:33–3:10",
+      "parts": [
+        "Pulleys can also be used to ",
+        {
+          "id": 13,
+          "answer": "lift things that are very heavy"
+        },
+        ". Now here we can see some sailors that are ",
+        {
+          "id": 14,
+          "answer": "pulling a net out of the ocean"
+        },
+        ". And up here, there's a big pulley. Now, if you imagine pulling a big net full of fish out of the ocean, if you just pulled it straight up, ",
+        {
+          "id": 15,
+          "answer": "it would take a lot of effort"
+        },
+        ". But if you have it hooked up to a pulley, ",
+        {
+          "id": 16,
+          "answer": "then they just need to pull down"
+        },
+        ". And pulling down is much easier than pulling straight up. And so, pulleys can make it a lot easier to lift heavy things. "
+      ]
+    },
+    {
+      "title": "One pulley and the jug",
+      "start": 190.78,
+      "end": 253.8,
+      "timeLabel": "3:10–4:13",
+      "parts": [
+        "Now let's do some experiments with pulleys and ",
+        {
+          "id": 17,
+          "answer": "see how they can help us to lift something"
+        },
+        " that's heavy. So here I have something that I want to lift, and it's kind of heavy. And up here I have a pulley to help me lift it. Now if I attach the thing I want to lift to a rope, imagine that I was trying to ",
+        {
+          "id": 18,
+          "answer": "move it up to a second story of a building"
+        },
+        ". Well trying to lift it straight up like this, It's quite difficult. But if I attach a pulley way up here and ",
+        {
+          "id": 19,
+          "answer": "put the rope around the pulley"
+        },
+        ", then it's much, much easier. I can just pull it right up and then it can rest wherever I want it to. It makes work a lot easier because instead of lifting straight up, ",
+        {
+          "id": 20,
+          "answer": "I can just pull down as the milk comes up"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "A system of pulleys",
+      "start": 256.66,
+      "end": 310.05,
+      "timeLabel": "4:16–5:10",
+      "parts": [
+        "Well, we can make work even easier using pulleys ",
+        {
+          "id": 21,
+          "answer": "if we use more than one pulley"
+        },
+        ". So ",
+        {
+          "id": 22,
+          "answer": "here I have four pulleys"
+        },
+        ". And when we have more than one pulley put together like this, ",
+        {
+          "id": 23,
+          "answer": "working together, then we call it a system of pulleys"
+        },
+        " or a pulley system. So I need to attach my weight, ",
+        {
+          "id": 24,
+          "answer": "the load that we want to lift"
+        },
+        ", to the pulleys. I wanna make a nice, good connection so that it doesn't slip around too much when I pull on the rope. "
+      ]
+    },
+    {
+      "title": "The load stays the same",
+      "start": 323.33000000000004,
+      "end": 345.44,
+      "timeLabel": "5:23–5:45",
+      "parts": [
+        "Now I'm ready to try it. ",
+        {
+          "id": 25,
+          "answer": "Do you think it will be any easier"
+        },
+        "? ",
+        {
+          "id": 26,
+          "answer": "It's much, much easier to raise the milk now"
+        },
+        ". And of course ",
+        {
+          "id": 27,
+          "answer": "the milk jug isn't any lighter"
+        },
+        ", but having four pulleys working together makes the work much, much easier to do. "
+      ]
+    },
+    {
+      "title": "Explain the advantage",
+      "start": 347.58000000000004,
+      "end": 361.35,
+      "timeLabel": "5:47–6:01",
+      "parts": [
+        "And so ",
+        {
+          "id": 28,
+          "answer": "when we need to lift very heavy things"
+        },
+        ", ",
+        {
+          "id": 29,
+          "answer": "if we use a system of pulleys"
+        },
+        ", it can make the work much easier. So pulleys are ",
+        {
+          "id": 30,
+          "answer": "another amazing simple machine"
+        },
+        ". "
+      ]
+    }
+  ]
+};
