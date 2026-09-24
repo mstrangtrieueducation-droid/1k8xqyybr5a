83e020478d3856100fd400252961f8c9673e9ff8
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 17:30:39 +0700

    Add reviewed Science G3 Video 45 lesson

---
+++ 97d7fb6214af/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-45",
+  "number": 45,
+  "title": "Computers",
+  "duration": 323.1573125,
+  "durationLabel": "5 phút 23 giây",
+  "total": 30,
+  "audio": "97d7fb6214af/audio.mp3",
+  "mindmap": "97d7fb6214af/mindmap.png",
+  "questionsImage": "97d7fb6214af/questions.jpg",
+  "speakingPoints": [
+    "Working with information",
+    "Chips around us",
+    "Designing new things",
+    "Finding a location",
+    "Sending data with light",
+    "Connected board parts"
+  ],
+  "questions": [
+    {
+      "text": "What three things can a computer do with information? How have the size and portability of many computers changed?"
+    },
+    {
+      "text": "Give two examples of everyday objects that use computer chips. Explain what a chip helps each object do."
+    },
+    {
+      "text": "Where do GPS receivers get their signals? How can a navigation device help someone find their location and plan a journey?"
+    },
+    {
+      "text": "How does an optical fiber carry information? Why are these thin strands useful in computer networks?"
+    },
+    {
+      "text": "What does the teacher show on the computer board? Explain why the tiny traces connecting its parts are important."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "What computers can do",
+      "start": 6.27,
+      "end": 43.5,
+      "timeLabel": "0:06–0:43",
+      "parts": [
+        "Today we're going to be talking about computers. Now computers are amazing and ",
+        {
+          "id": 1,
+          "answer": "computer technology is everywhere"
+        },
+        ". A computer can ",
+        {
+          "id": 2,
+          "answer": "store, process, and send electronic information"
+        },
+        " incredibly fast. If you've ever used a computer, you know that they can do things very quickly. Computers have changed a lot over the years. Now, a long time ago, computers used to be very, very large and they'd have specific purposes. ",
+        {
+          "id": 3,
+          "answer": "People didn't all have a computer in their homes"
+        },
+        ". But over the years, computers changed. "
+      ]
+    },
+    {
+      "title": "Smaller and portable computers",
+      "start": 43.75,
+      "end": 77.75,
+      "timeLabel": "0:43–1:17",
+      "parts": [
+        "They became ",
+        {
+          "id": 4,
+          "answer": "smaller and also faster"
+        },
+        " and able to do more things. Now, many of the computers we see are ",
+        {
+          "id": 5,
+          "answer": "laptops or even small tablets"
+        },
+        ". And ",
+        {
+          "id": 6,
+          "answer": "we can carry these around with us"
+        },
+        ". They're so small. And they can also be very fast and able to do a lot of things. So computer technology has changed a lot throughout the years. Now computers are amazing, but we actually use computer technology in many different things besides just what we call computers. "
+      ]
+    },
+    {
+      "title": "Chips in everyday objects",
+      "start": 78.82,
+      "end": 124.54,
+      "timeLabel": "1:18–2:04",
+      "parts": [
+        "A digital watch ",
+        {
+          "id": 7,
+          "answer": "keeps track of the time with a computer chip"
+        },
+        ". So if you have a digital watch, you know that inside that watch is a little computer chip, and that's how a digital watch keeps track of the time. ",
+        {
+          "id": 8,
+          "answer": "Calculators, cameras, phones, and cars"
+        },
+        " all use computer chips. So all these things that we use so often, they use computer chips to work. ",
+        {
+          "id": 9,
+          "answer": "Even some toys use computer chips"
+        },
+        ". Some of the toys that you can push buttons on and they ",
+        {
+          "id": 10,
+          "answer": "make noise or play music"
+        },
+        ", they use computer chips. There's some little computer chips inside the toy that make it do whatever it does. If it lights up, then that toy probably uses a computer chip. "
+      ]
+    },
+    {
+      "title": "Designing with computers",
+      "start": 127.49,
+      "end": 137.94,
+      "timeLabel": "2:07–2:17",
+      "parts": [
+        "Computers can help us to ",
+        {
+          "id": 11,
+          "answer": "design new products"
+        },
+        ". So computers can help us to design new technology. We can use computers to ",
+        {
+          "id": 12,
+          "answer": "design many different things"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "GPS and satellites",
+      "start": 141.57999999999998,
+      "end": 186.95000000000002,
+      "timeLabel": "2:21–3:06",
+      "parts": [
+        "GPS computers can ",
+        {
+          "id": 13,
+          "answer": "use signals from satellites"
+        },
+        " to give us information, such as our exact location. If you've ever used a GPS computer, then you can ",
+        {
+          "id": 14,
+          "answer": "figure out where you are right away"
+        },
+        ", and it can also help you to figure out ",
+        {
+          "id": 15,
+          "answer": "how to get to where you want to go"
+        },
+        ". And how does the GPS computer get its information? ",
+        {
+          "id": 16,
+          "answer": "From satellites that orbit the Earth"
+        },
+        ". So this new technology can help us a lot. If you're lost, if you have a GPS computer, it can help you so that you're not lost. They're really amazing and they use their information from the satellites that go around the earth. "
+      ]
+    },
+    {
+      "title": "Optical fibers and information",
+      "start": 188.10999999999999,
+      "end": 223.79000000000002,
+      "timeLabel": "3:08–3:43",
+      "parts": [
+        "Optical fibers are ",
+        {
+          "id": 17,
+          "answer": "making computer networks work better"
+        },
+        ". Now an optical fiber is a thin strand of bendable glass and ",
+        {
+          "id": 18,
+          "answer": "they send information using light"
+        },
+        ". Now one nice thing about optical fibers is they don't get as hot as wires and also they take up less space. A single optical fiber can carry more data than hundreds of wires twisted into a fat cable. So just a single one of these tiny optical fiber strands ",
+        {
+          "id": 19,
+          "answer": "can carry a lot of data"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Look at a computer board",
+      "start": 227.39,
+      "end": 255.95000000000002,
+      "timeLabel": "3:47–4:15",
+      "parts": [
+        "Now today ",
+        {
+          "id": 20,
+          "answer": "I brought a computer board with me"
+        },
+        ". Now you can see that this computer board has many different parts on it. It even has some parts connected to it that can be ",
+        {
+          "id": 21,
+          "answer": "used to plug different things in"
+        },
+        ". This particular computer board ",
+        {
+          "id": 22,
+          "answer": "comes from a server"
+        },
+        ". See how there's many, many different parts on it? And each of these parts have their own different things that they do to make the computer work the right way. "
+      ]
+    },
+    {
+      "title": "Traces connect the parts",
+      "start": 259.22,
+      "end": 286.95,
+      "timeLabel": "4:19–4:46",
+      "parts": [
+        "If you look at the back of it, ",
+        {
+          "id": 23,
+          "answer": "you can see even more parts"
+        },
+        ". But there's one thing that I want you to notice. If you look very closely, you can see tiny traces, ",
+        {
+          "id": 24,
+          "answer": "tiny lines through the computer board"
+        },
+        ". Well, ",
+        {
+          "id": 25,
+          "answer": "these lines actually connect different parts together"
+        },
+        ". They're tiny wires that connect different parts in the computer board together to make them all work properly. "
+      ]
+    },
+    {
+      "title": "Layers of a board",
+      "start": 288.43,
+      "end": 298.21,
+      "timeLabel": "4:48–4:58",
+      "parts": [
+        "",
+        {
+          "id": 26,
+          "answer": "Computer boards can have different layers"
+        },
+        ". They can be very, very complicated. But of course they can. ",
+        {
+          "id": 27,
+          "answer": "Computers can do so many different things"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Learning leads to new uses",
+      "start": 300.32000000000005,
+      "end": 316.89,
+      "timeLabel": "5:00–5:16",
+      "parts": [
+        "So ",
+        {
+          "id": 28,
+          "answer": "computer technology is amazing"
+        },
+        ". And it's everywhere. And a really amazing thing about technology is that ",
+        {
+          "id": 29,
+          "answer": "the more we learn, the more we are able to do"
+        },
+        ". And the more things that we learn about computers, ",
+        {
+          "id": 30,
+          "answer": "the more things we'll be able to use computers to do"
+        },
+        ". "
+      ]
+    }
+  ]
+};
