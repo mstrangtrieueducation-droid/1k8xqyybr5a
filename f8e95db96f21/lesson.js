8b17512ce40fdb88099dd8ea6948f6f70922caa3
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 17:08:36 +0700

    Add reviewed Science G3 Video 43 lesson

---
+++ f8e95db96f21/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-43",
+  "number": 43,
+  "title": "How Energy Changes Form",
+  "duration": 313.4461875,
+  "durationLabel": "5 phút 13 giây",
+  "total": 30,
+  "audio": "f8e95db96f21/audio.mp3",
+  "mindmap": "f8e95db96f21/mindmap.png",
+  "questionsImage": "f8e95db96f21/questions.jpg",
+  "speakingPoints": [
+    "Electrical energy in",
+    "Light and heat out",
+    "Energy in batteries",
+    "A moving toothbrush",
+    "Energy in our bodies",
+    "Explain everyday actions"
+  ],
+  "questions": [
+    {
+      "text": "What energy does the lamp receive, and what forms does it give off? Which observations in the video support your answer?"
+    },
+    {
+      "text": "What kind of energy is stored in the toothbrush batteries? Trace the changes from the batteries to the moving, noisy toothbrush."
+    },
+    {
+      "text": "Compare the lamp and electric toothbrush. What form of energy powers each device, and how do their main outputs differ?"
+    },
+    {
+      "text": "How does food help people move and stay warm? Use the soccer example to explain the energy changes."
+    },
+    {
+      "text": "When the teacher moves her hand to turn on the lamp, what energy change happens in her body? What separate changes happen in the lamp?"
+    }
+  ],
+  "chunks": [
+    {
+      "title": "Look at a familiar action",
+      "start": 6.3,
+      "end": 26.47,
+      "timeLabel": "0:06–0:26",
+      "parts": [
+        "Today we're going to start by ",
+        {
+          "id": 1,
+          "answer": "doing something that's very common"
+        },
+        ". In fact, it's something that probably most of you do every single night. ",
+        {
+          "id": 2,
+          "answer": "We're going to be turning on a lamp"
+        },
+        ". But as we do this today, I want you to think about ",
+        {
+          "id": 3,
+          "answer": "some of the things that we've learned about energy"
+        },
+        ". And we're going to be thinking about what's happening here in a whole different way. "
+      ]
+    },
+    {
+      "title": "The lamp's energy supply",
+      "start": 27.720000000000002,
+      "end": 55.15,
+      "timeLabel": "0:27–0:55",
+      "parts": [
+        "So, first let's talk about this lamp. ",
+        {
+          "id": 4,
+          "answer": "This lamp has a cord"
+        },
+        ". And this cord... It's connected to a plug that plugs into the outlet in the wall. Now we know that ",
+        {
+          "id": 5,
+          "answer": "we must plug in the lamp in order for it to work"
+        },
+        " because ",
+        {
+          "id": 6,
+          "answer": "the lamp runs off of electricity"
+        },
+        ". And the lamp gets the electricity from the outlet in the wall because it's hooked up to electricity. So when we plug in things, they get that electricity. "
+      ]
+    },
+    {
+      "title": "Light and heat from electricity",
+      "start": 55.730000000000004,
+      "end": 105.10000000000001,
+      "timeLabel": "0:55–1:45",
+      "parts": [
+        "So what happens when we flip the switch? Well, we all know the lamp will turn on. Well, when I flip the switch, Of course, the lamp turns on. But let's think about what's really happening here. ",
+        {
+          "id": 7,
+          "answer": "Electricity is flowing up the cord"
+        },
+        ", and it's making the lamp be able to work. But the lamp is changing this electrical energy into a new kind of energy, light energy. And it's also ",
+        {
+          "id": 8,
+          "answer": "giving off some heat energy"
+        },
+        ". If I put my hand close to the lamp, I can feel warmth. Of course, you never want to touch lamps that have been on for a while because the light bulbs get very, very hot. So what's happening is electrical energy here is ",
+        {
+          "id": 9,
+          "answer": "changing into light energy and heat energy"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Energy changes form",
+      "start": 107.13,
+      "end": 126.5,
+      "timeLabel": "1:47–2:06",
+      "parts": [
+        "So ",
+        {
+          "id": 10,
+          "answer": "what were we able to learn from this little example"
+        },
+        "? Energy can change from one form to another. ",
+        {
+          "id": 11,
+          "answer": "This is a really important thing to understand"
+        },
+        ". ",
+        {
+          "id": 12,
+          "answer": "When we turned on the lamp"
+        },
+        ", electrical energy changed. into light energy and heat energy. Well, let's look at another example. "
+      ]
+    },
+    {
+      "title": "A battery-powered toothbrush",
+      "start": 126.72999999999999,
+      "end": 156.82,
+      "timeLabel": "2:06–2:36",
+      "parts": [
+        "Here I have a toothbrush, and ",
+        {
+          "id": 13,
+          "answer": "this toothbrush is powered by batteries"
+        },
+        ". Now remember, ",
+        {
+          "id": 14,
+          "answer": "batteries have stored potential chemical energy"
+        },
+        ". So there's potential energy in these batteries, and it's chemical energy. But ",
+        {
+          "id": 15,
+          "answer": "when we put these batteries into the toothbrush"
+        },
+        ", now this toothbrush can work. It can use that. potential energy that's in the batteries to make something happen. "
+      ]
+    },
+    {
+      "title": "Motion and sound",
+      "start": 157.53,
+      "end": 205.42000000000002,
+      "timeLabel": "2:37–3:25",
+      "parts": [
+        "Well, when I turn this toothbrush on, it does something. Let's talk about what happens here. ",
+        {
+          "id": 16,
+          "answer": "There's chemical energy inside the batteries"
+        },
+        ", but this chemical energy actually changes to electrical energy to power the toothbrush. And then, that electrical energy makes, when I turn on the switch, it makes the toothbrush move, and it also makes it sound. What are the different types of energy that we're seeing here? Chemical energy, ",
+        {
+          "id": 17,
+          "answer": "changes to electrical energy, which changes to energy of motion"
+        },
+        ", and energy of sound. Isn't that amazing? ",
+        {
+          "id": 18,
+          "answer": "All of these different changes happen in just this one little toothbrush"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Energy from food",
+      "start": 207.35,
+      "end": 243.08,
+      "timeLabel": "3:27–4:03",
+      "parts": [
+        "Now it's really important that energy is able to change from one form to another. If it wasn't possible for energy to do that, then ",
+        {
+          "id": 19,
+          "answer": "we wouldn't be able to survive"
+        },
+        ". Let's look at an example. When people eat food, and when animals eat food, then our bodies are able to ",
+        {
+          "id": 20,
+          "answer": "take the stored potential energy in that food"
+        },
+        ", and then we're able to use it to do things. When we eat food, ",
+        {
+          "id": 21,
+          "answer": "we get chemical energy from the food"
+        },
+        ", and then we can change it into energy of motion when we move, and also heat energy. "
+      ]
+    },
+    {
+      "title": "Moving and getting warm",
+      "start": 243.59,
+      "end": 274.27,
+      "timeLabel": "4:03–4:34",
+      "parts": [
+        "And you can feel this heat energy a lot after you play a good game. a good game of ",
+        {
+          "id": 22,
+          "answer": "some sport like soccer"
+        },
+        ", ",
+        {
+          "id": 23,
+          "answer": "you get all hot and sweaty"
+        },
+        ". Well, that's the heat energy coming off of your body. And so we can use this chemical energy from the food to get energy of motion and heat energy. And if you think about it, if it weren't for that, we wouldn't be able to survive. We wouldn't be able to move at all. And our bodies wouldn't be able to ",
+        {
+          "id": 24,
+          "answer": "stay the nice warm temperature that they need"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Notice energy changes",
+      "start": 274.91,
+      "end": 287.5,
+      "timeLabel": "4:34–4:47",
+      "parts": [
+        "So today we've ",
+        {
+          "id": 25,
+          "answer": "learned a very important thing about energy"
+        },
+        ". ",
+        {
+          "id": 26,
+          "answer": "Energy can change from one form to another"
+        },
+        ". And it's really interesting to think about this ",
+        {
+          "id": 27,
+          "answer": "as you do different things throughout the day"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "The body and lamp together",
+      "start": 288.47,
+      "end": 308.0,
+      "timeLabel": "4:48–5:08",
+      "parts": [
+        "Even when we do things like turn on the lamp, ",
+        {
+          "id": 28,
+          "answer": "when I move my hand to turn the switch"
+        },
+        ", ",
+        {
+          "id": 29,
+          "answer": "my body is changing chemical energy to energy of motion"
+        },
+        ". When I turn on that switch, electrical energy is changing into light and heat energy. ",
+        {
+          "id": 30,
+          "answer": "Energy is changing forms all the time"
+        },
+        ". "
+      ]
+    }
+  ]
+};
