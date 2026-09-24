ac215d69005b6d95798790606dd6b6ce7f23bd65
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 18:13:48 +0700

    Add reviewed Science G3 Video 48 lesson

---
+++ f906b8b51787/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-48",
+  "number": 48,
+  "title": "Exploring Clouds",
+  "duration": 359.491375,
+  "durationLabel": "5 phút 59 giây",
+  "total": 30,
+  "audio": "f906b8b51787/audio.mp3",
+  "mindmap": "f906b8b51787/mindmap.png",
+  "questionsImage": "f906b8b51787/questions.jpg",
+  "speakingPoints": [
+    "Tiny droplets or ice",
+    "The warm jar model",
+    "Cooling and particles",
+    "A visible cloud",
+    "Shapes on fair days",
+    "Clues about weather"
+  ],
+  "questions": [
+    {
+      "text": "What are clouds made of? How is a visible cloud different from invisible water vapor?"
+    },
+    {
+      "text": "Why does the teacher use warm water and ice in the jar model? Explain the different job of each."
+    },
+    {
+      "text": "What role do the tiny smoke particles play? Explain how water droplets form around them when the moist air cools."
+    },
+    {
+      "text": "What does the teacher observe inside the jar and when she lifts the lid? How do these observations help us understand a cloud?"
+    },
+    {
+      "text": "Compare small cumulus clouds, cirrus clouds, and the dark storm clouds shown. What shapes or colors give us clues about the weather?"
+    }
+  ],
+  "chunks": [
+    {
+      "title": "What a cloud is",
+      "start": 0.22,
+      "end": 48.54,
+      "timeLabel": "0:00–0:48",
+      "parts": [
+        "Today we're going to be talking about clouds, but first let's remember what clouds are. A cloud is a ",
+        {
+          "id": 1,
+          "answer": "visible body of tiny water droplets"
+        },
+        " or ice crystals suspended in the air. So we know that clouds are made of tiny water droplets or ice crystals. Now it's one thing to talk about what a cloud is made of, but sometimes it helps even more if we can ",
+        {
+          "id": 2,
+          "answer": "really see how a cloud forms"
+        },
+        ". So today we're going to be ",
+        {
+          "id": 3,
+          "answer": "making a cloud form inside of this glass jar"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "A supervised demonstration",
+      "start": 49.550000000000004,
+      "end": 75.06,
+      "timeLabel": "0:49–1:15",
+      "parts": [
+        "Now ",
+        {
+          "id": 4,
+          "answer": "before we even get started"
+        },
+        ", I need to remind you of something. ",
+        {
+          "id": 5,
+          "answer": "If you ever try to do this lab yourself"
+        },
+        ", you need to make sure that you have adult supervision. We're going to be dealing with some things that are quite dangerous, such as ",
+        {
+          "id": 6,
+          "answer": "boiling water and matches"
+        },
+        ". And you need to make sure that you have adult supervision if you're going to do this lab and use these things. All right, let's get started. "
+      ]
+    },
+    {
+      "title": "Warm water in the jar",
+      "start": 76.49,
+      "end": 116.68,
+      "timeLabel": "1:16–1:56",
+      "parts": [
+        "So here I have ",
+        {
+          "id": 7,
+          "answer": "some very hot water"
+        },
+        ". You can see that it's even boiling. You can see these little bubbles coming up. It's very hot. I'm going to take this hot water, I'm ",
+        {
+          "id": 8,
+          "answer": "using hot pads because it's so hot"
+        },
+        ", and carefully pour it. into this jar. Now I'm going to cover the jar. You can see all the steam that is immediately coming up. This jar is being filled with steam. I'm going to let this hot water ",
+        {
+          "id": 9,
+          "answer": "sit in the jar for just a few minutes"
+        },
+        ", and then we'll continue with the experiment. "
+      ]
+    },
+    {
+      "title": "Make the inside easier to see",
+      "start": 118.14999999999999,
+      "end": 154.63,
+      "timeLabel": "1:58–2:34",
+      "parts": [
+        "All right, it's been a few minutes, so let's continue our experiment. We're going to take the lid off the jar and ",
+        {
+          "id": 10,
+          "answer": "pour out most of this hot water"
+        },
+        ". just ",
+        {
+          "id": 11,
+          "answer": "leaving a small amount of the water left in the jar"
+        },
+        ". Add a little bit more and a little bit left in there. Okay. And now I want to kind of swish around this water to ",
+        {
+          "id": 12,
+          "answer": "get the water droplets off the sides"
+        },
+        " so you'll be able to see inside the jar better. All right. "
+      ]
+    },
+    {
+      "title": "Smoke and a cold lid",
+      "start": 155.39,
+      "end": 190.89000000000001,
+      "timeLabel": "2:35–3:10",
+      "parts": [
+        "Now ",
+        {
+          "id": 13,
+          "answer": "we need to add some smoke"
+        },
+        ". We'll use a match. And remember, if you do any of these, you need adult supervision. And now... We need to make it cool up here at the top. So I'm going to ",
+        {
+          "id": 14,
+          "answer": "add some ice cubes to the top of the lid"
+        },
+        ". Now, so that you can see it better, I'm going to ",
+        {
+          "id": 15,
+          "answer": "put some black poster board behind our jar"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Droplets form around particles",
+      "start": 191.97,
+      "end": 228.67000000000002,
+      "timeLabel": "3:11–3:48",
+      "parts": [
+        "Do you see the cloud that's been formed? What's happening is the ",
+        {
+          "id": 16,
+          "answer": "warm water vapor that was inside this jar"
+        },
+        " is rising up to the cool air up the top of the jar. And that water vapor is ",
+        {
+          "id": 17,
+          "answer": "condensing around tiny smoke particles"
+        },
+        ", forming small drops of water. Now these droplets of water are making a cloud. And that's ",
+        {
+          "id": 18,
+          "answer": "the cloud that we can see swirling around inside of here"
+        },
+        ". Those small droplets of water that have condensed around the particles of dust in the cool air. "
+      ]
+    },
+    {
+      "title": "The model and clouds outside",
+      "start": 229.31,
+      "end": 260.29,
+      "timeLabel": "3:49–4:20",
+      "parts": [
+        "You can see the cloud is just ",
+        {
+          "id": 19,
+          "answer": "getting thicker and thicker and swirling around"
+        },
+        ". And the jar is becoming more and more cloudy with these tiny water droplets. Now up in the sky when clouds form, ",
+        {
+          "id": 20,
+          "answer": "the water vapor is cooled up in the cool air"
+        },
+        ", and it condensates around tiny dust particles in the air, forming those tiny water droplets. And ",
+        {
+          "id": 21,
+          "answer": "those water droplets make up a cloud"
+        },
+        ", similar to what we've created here, a cloud. "
+      ]
+    },
+    {
+      "title": "See the cloud leave the jar",
+      "start": 260.78000000000003,
+      "end": 288.97,
+      "timeLabel": "4:20–4:48",
+      "parts": [
+        "Now, so that you can really see this, I'm going to ",
+        {
+          "id": 22,
+          "answer": "take off the lid"
+        },
+        " of the jar and ",
+        {
+          "id": 23,
+          "answer": "let some of this cloud out"
+        },
+        ". Watch carefully. You see that cloud? And if I put my hand over the jar, it actually feels wet and makes tiny water droplets on my hand because ",
+        {
+          "id": 24,
+          "answer": "this cloud is made of water droplets"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Recognize cloud shapes",
+      "start": 291.47,
+      "end": 323.96,
+      "timeLabel": "4:51–5:23",
+      "parts": [
+        "Now there are many different kinds of clouds. Some clouds form in sunny weather while other clouds form in stormy weather. And because of this, ",
+        {
+          "id": 25,
+          "answer": "we can use clouds to help us predict the weather"
+        },
+        ". Let's take a look. Here are some of the clouds that can form on sunny days. Cumulus clouds, they look kind of like cotton, ",
+        {
+          "id": 26,
+          "answer": "puffs of cotton"
+        },
+        ". Notice that they're very white. And cirrus clouds, they're also very white and ",
+        {
+          "id": 27,
+          "answer": "they look kind of like feathers"
+        },
+        ". They're actually made of tiny ice crystals. "
+      ]
+    },
+    {
+      "title": "Clouds give weather clues",
+      "start": 325.69,
+      "end": 356.78000000000003,
+      "timeLabel": "5:25–5:56",
+      "parts": [
+        "Now ",
+        {
+          "id": 28,
+          "answer": "other types of clouds form on stormy days"
+        },
+        ". And ",
+        {
+          "id": 29,
+          "answer": "these clouds are normally very dark"
+        },
+        ". And if you go outside and you see a sky that looks like this with many dark clouds, then you can normally know that if a storm hasn't started yet, it probably will. So clouds are really amazing and important to Earth. They're made of tiny water droplets or ice crystals. And ",
+        {
+          "id": 30,
+          "answer": "paying attention to the clouds can help us make predictions about the weather"
+        },
+        ". "
+      ]
+    }
+  ]
+};
