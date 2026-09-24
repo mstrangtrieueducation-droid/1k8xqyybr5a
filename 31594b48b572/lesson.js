0ab36073f68412df88c28ebd7d92e90be98cb53b
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 15:41:43 +0700

    Add reviewed Science G3 Video 34 lesson

---
+++ 31594b48b572/lesson.js
@@ -0,0 +1,260 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-34",
+  "number": 34,
+  "title": "Gravity",
+  "duration": 275.226125,
+  "durationLabel": "4 phút 35 giây",
+  "total": 30,
+  "audio": "31594b48b572/audio.mp3",
+  "mindmap": "31594b48b572/mindmap.png",
+  "questionsImage": "31594b48b572/questions.jpg",
+  "speakingPoints": [
+    "A pull without contact",
+    "Contact forces",
+    "Falling toward Earth",
+    "Mass",
+    "Weight",
+    "Earth and Moon"
+  ],
+  "questions": [
+    {
+      "text": "What is gravity, and why is it called a non-contact force? Use a dropped toy to explain."
+    },
+    {
+      "text": "How is gravity different from pushing an object with your hand or from friction? Which interactions need touching surfaces?"
+    },
+    {
+      "text": "What pulls the parachutist toward Earth? What does the parachute do to the fall, and does gravity stop acting?"
+    },
+    {
+      "text": "What is the difference between mass and weight? Why can the same object have different weights in different places?"
+    },
+    {
+      "text": "If you took the same object from Earth to the Moon, what would happen to its mass and weight? Explain using the Moon's weaker gravity."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "Why things fall",
+      "start": 12.9,
+      "end": 47.089999999999996,
+      "timeLabel": "0:12–0:47",
+      "parts": [
+        "Today we're going to be studying gravity. Now gravity is something that you've been studying ever since you were a baby. We all know how tiny babies will ",
+        {
+          "id": 1,
+          "answer": "pick up a toy and drop it"
+        },
+        " just to watch their parent or babysitter pick it back up and give it to them so they can drop it again. Well we know that ",
+        {
+          "id": 2,
+          "answer": "when we drop an object"
+        },
+        " ",
+        {
+          "id": 3,
+          "answer": "it will go towards the ground"
+        },
+        ". And why? Because of gravity. But what exactly is gravity? How does it work? Gravity is ",
+        {
+          "id": 4,
+          "answer": "a non-contact force that pulls objects towards each other"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Forces that need contact",
+      "start": 47.9,
+      "end": 80.44000000000001,
+      "timeLabel": "0:47–1:20",
+      "parts": [
+        "Now, non-contact force. What could that be? We know that a contact force is a force where ",
+        {
+          "id": 5,
+          "answer": "we must make contact"
+        },
+        " to be able to apply the force. If I want to push something, ",
+        {
+          "id": 6,
+          "answer": "I have to touch it to be able to push it"
+        },
+        ". And with friction... If friction is going to happen, ",
+        {
+          "id": 7,
+          "answer": "two objects must rub against each other"
+        },
+        ". And to rub against each other, ",
+        {
+          "id": 8,
+          "answer": "they have to be touching"
+        },
+        ". They have to be making contact. So we understand contact forces, but what do you think is a non-contact force? "
+      ]
+    },
+    {
+      "title": "Pulling without touching",
+      "start": 81.13,
+      "end": 105.17,
+      "timeLabel": "1:21–1:45",
+      "parts": [
+        "Well, a non-contact force is a force that ",
+        {
+          "id": 9,
+          "answer": "can push or pull an object without touching it"
+        },
+        ". And ",
+        {
+          "id": 10,
+          "answer": "gravity is a non-contact force"
+        },
+        ". If you think about it, if we drop something, There's nothing touching the thing, except for the air, but somehow ",
+        {
+          "id": 11,
+          "answer": "it's still pulled towards the earth"
+        },
+        ". Well, that's because of gravity. "
+      ]
+    },
+    {
+      "title": "A parachutist falls toward Earth",
+      "start": 109.35,
+      "end": 149.63,
+      "timeLabel": "1:49–2:29",
+      "parts": [
+        "Gravity is what ",
+        {
+          "id": 12,
+          "answer": "pulls this parachuting person towards earth"
+        },
+        ". You can see this person who's coming down in a parachute. Now, if it weren't for gravity, what would happen to this person? They would probably just float away. But because of gravity, ",
+        {
+          "id": 13,
+          "answer": "this person will be pulled towards the earth"
+        },
+        ". Now again, notice that Gravity isn't necessarily touching this person. The only things that he's touching are the parachute, which is ",
+        {
+          "id": 14,
+          "answer": "helping to slow him down"
+        },
+        " so he doesn't hit the ground too hard. And it's the air around him, his clothes, there's nothing else touching him. But still, ",
+        {
+          "id": 15,
+          "answer": "gravity is pulling him"
+        },
+        " towards the Earth. It's a non-contact force. "
+      ]
+    },
+    {
+      "title": "Mass and weight",
+      "start": 152.5,
+      "end": 179.6,
+      "timeLabel": "2:32–2:59",
+      "parts": [
+        "",
+        {
+          "id": 16,
+          "answer": "Gravity affects weight"
+        },
+        ". Now what exactly is weight? Well, weight is a combination of ",
+        {
+          "id": 17,
+          "answer": "your mass, which is how much matter you contain"
+        },
+        ", and also ",
+        {
+          "id": 18,
+          "answer": "how much gravity is pulling you down"
+        },
+        ". If you think about it, ",
+        {
+          "id": 19,
+          "answer": "if you stand on a scale"
+        },
+        ", you have a mass, how much matter you contain, and then, since we're on Earth, there's gravity pulling us down, and that gravity pulls us down on the scale, and we have our weight. "
+      ]
+    },
+    {
+      "title": "The Moon has weaker gravity",
+      "start": 180.39,
+      "end": 210.84,
+      "timeLabel": "3:00–3:30",
+      "parts": [
+        "Well, if we went to the moon, ",
+        {
+          "id": 20,
+          "answer": "we'd still have the same amount of mass"
+        },
+        ", but... our weight would change. Why? Because ",
+        {
+          "id": 21,
+          "answer": "there's less gravity on the moon"
+        },
+        ". Now ",
+        {
+          "id": 22,
+          "answer": "the Earth is very large"
+        },
+        ", and so it has a lot of gravitational force pulling objects toward it, toward its center actually. But on the moon, there's not as much gravitational force. And notice that the moon is much smaller. ",
+        {
+          "id": 23,
+          "answer": "It still has some gravity"
+        },
+        ", but not as much as Earth. "
+      ]
+    },
+    {
+      "title": "Less weight, same mass",
+      "start": 211.48999999999998,
+      "end": 246.09,
+      "timeLabel": "3:31–4:06",
+      "parts": [
+        "So if you went and weighed on the moon, you'd actually only weigh ",
+        {
+          "id": 24,
+          "answer": "about one sixth of the amount"
+        },
+        " that you weigh on earth. So if you weighed on earth and you weighed about 60 pounds, and then you went to the moon, you had your scale, and you weighed on the moon, you would only weigh about 10 pounds. And ",
+        {
+          "id": 25,
+          "answer": "your mass would still be the same"
+        },
+        ", they'd still be the same height, you'd look the same, ",
+        {
+          "id": 26,
+          "answer": "you'd contain the same amount of matter"
+        },
+        ", but ",
+        {
+          "id": 27,
+          "answer": "there'd be less gravity pulling you down"
+        },
+        ". And so your weight would only be one sixth the amount. only about one sixth. "
+      ]
+    },
+    {
+      "title": "Earth pulls toward its center",
+      "start": 247.21,
+      "end": 272.62,
+      "timeLabel": "4:07–4:32",
+      "parts": [
+        "Well, ",
+        {
+          "id": 28,
+          "answer": "gravity is an amazing force"
+        },
+        " and as you learn more and more about science, you'll learn more about this gravity and more of the reasons why it's so important. But what I want you to remember today is that gravity is a non-contact force and gravity is what ",
+        {
+          "id": 29,
+          "answer": "pulls everything on earth towards its center"
+        },
+        ". It's what ",
+        {
+          "id": 30,
+          "answer": "pulls everything towards the ground"
+        },
+        ". Without gravity, things would just float away. "
+      ]
+    }
+  ]
+};
