5337d6773770689c30cf9ed0d5c9f12899b826c3
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 17:39:26 +0700

    Add reviewed Science G3 Video 46 lesson

---
+++ c6fd67714f62/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-46",
+  "number": 46,
+  "title": "Earth's Water",
+  "duration": 582.3100625,
+  "durationLabel": "9 phút 42 giây",
+  "total": 30,
+  "audio": "c6fd67714f62/audio.mp3",
+  "mindmap": "c6fd67714f62/mindmap.png",
+  "questionsImage": "c6fd67714f62/questions.jpg",
+  "speakingPoints": [
+    "A model for comparison",
+    "Air and surface water",
+    "Water underground",
+    "Ice caps and glaciers",
+    "Oceans hold the most",
+    "Use freshwater wisely"
+  ],
+  "questions": [
+    {
+      "text": "What does the water in the glass dish represent? Why does the teacher add blue coloring before measuring it into cups?"
+    },
+    {
+      "text": "Compare the atmosphere cup with the lakes, rivers, and streams cup. What do the tiny amounts tell us about Earth's water?"
+    },
+    {
+      "text": "What is groundwater, and how can water get underground? Explain how people can bring some of it to the surface."
+    },
+    {
+      "text": "Which freshwater group has the largest amount in the model? How do 30 mL, 15 mL, and 2 mL add up to the amount needed?"
+    },
+    {
+      "text": "What does the large amount left in the bowl represent? Why should people protect freshwater even though Earth has so much water?"
+    }
+  ],
+  "chunks": [
+    {
+      "title": "Our blue planet",
+      "start": 12.66,
+      "end": 81.26,
+      "timeLabel": "0:12–1:21",
+      "parts": [
+        "Today we're going to be talking about Earth's water. Now Earth could be called the blue planet because ",
+        {
+          "id": 1,
+          "answer": "from space it looks blue"
+        },
+        ". But what gives Earth this blue color? That's right, it's all this water that is on Earth. The water makes it look blue from space. About 3 fourths or 75% of the Earth's surface is covered with water. Now we can show this with a circle graph. Here you can see ",
+        {
+          "id": 2,
+          "answer": "this whole circle represents all of Earth's surface"
+        },
+        ". And this much of Earth's surface is covered with water. ",
+        {
+          "id": 3,
+          "answer": "Only this much of Earth's surface is dry land"
+        },
+        ". So you can see there is much more water covering Earth's surface than there is dry land. And if you look at the Earth from space, you can see there's a lot more of this blue color than there is of these land places. If you just look at the earth you can see there's a lot more water covering the surface of earth. "
+      ]
+    },
+    {
+      "title": "Make a model of Earth's water",
+      "start": 82.16,
+      "end": 154.52,
+      "timeLabel": "1:22–2:34",
+      "parts": [
+        "Now I want you to imagine that ",
+        {
+          "id": 4,
+          "answer": "all of earth's water is right here in this glass dish"
+        },
+        ". Now of course there's much more water than this on earth, but I want you to imagine this water represents all of the water on earth. Now this table shows ",
+        {
+          "id": 5,
+          "answer": "different places where water can be found on earth"
+        },
+        " and the different types of water that are on Earth that can be found in these places. So we're going to separate the water in the glass dish into each of these different places so that we can see how the water is in different places on Earth. Let's get started. Now because water is clear, it may be hard to see when I measure it into the small amounts. So I'm going to ",
+        {
+          "id": 6,
+          "answer": "add some color so that you'll be able to see it better"
+        },
+        ". I'm going to add some blue coloring so you'll be able to see the water better from the camera. And remember, this just represents normal water on Earth. And I'll stir it up, mixing in all that color. Let's add a little bit more. "
+      ]
+    },
+    {
+      "title": "Water in the atmosphere",
+      "start": 168.28,
+      "end": 221.28,
+      "timeLabel": "2:48–3:41",
+      "parts": [
+        "All right, so first in our table, we have the atmosphere. And this is fresh water. Now, what is the atmosphere? And how does it have water in it? Well, let's look at a definition of the word. Atmosphere. Atmosphere, ",
+        {
+          "id": 7,
+          "answer": "the whole mass of air surrounding the earth"
+        },
+        ". So if you think about all of the air that surrounds the earth, that is the atmosphere. And you know there's air all around us, we can breathe it. It's the atmosphere, all of the air surrounding the earth. Now ",
+        {
+          "id": 8,
+          "answer": "water can evaporate and go into the atmosphere"
+        },
+        ". And ",
+        {
+          "id": 9,
+          "answer": "there's actually water vapor in the atmosphere"
+        },
+        ", and we'll be talking more about that in later lessons as we study water. But I want you to know there is some water floating around us in the atmosphere, in the air around us, in the air that surrounds Earth. "
+      ]
+    },
+    {
+      "title": "Compare tiny amounts",
+      "start": 226.62,
+      "end": 291.12,
+      "timeLabel": "3:46–4:51",
+      "parts": [
+        "And how much water do we need to put for the atmosphere? ",
+        {
+          "id": 10,
+          "answer": "Only about one half of a drop"
+        },
+        ". So here I have a cup that's labeled atmosphere and we only need one half of a drop I'm going to try and get just half of a drop. That is such a tiny amount. You can hardly see it because there's not very much water in our atmosphere. It's just a very little amount. All right. So next we have lakes rivers and streams. These are also fresh water, and in this cup we need to put about four drops. So here we have lakes, rivers, and streams. And I'm going to ",
+        {
+          "id": 11,
+          "answer": "take my dropper and drop in four drops"
+        },
+        ". One, two, three, four. And again, that's ",
+        {
+          "id": 12,
+          "answer": "not very much water compared to all the water on Earth"
+        },
+        ". But it is more than how much water we have in the atmosphere. "
+      ]
+    },
+    {
+      "title": "Groundwater",
+      "start": 292.45000000000005,
+      "end": 317.67,
+      "timeLabel": "4:52–5:17",
+      "parts": [
+        "Well, next we have ground water. And this is also fresh water. But what exactly is ground water? Well, groundwater is ",
+        {
+          "id": 13,
+          "answer": "water that's underground"
+        },
+        ". Rain and melting ice seep through the ground and ",
+        {
+          "id": 14,
+          "answer": "collect in spaces between rocks"
+        },
+        ". And people can ",
+        {
+          "id": 15,
+          "answer": "dig wells to help bring this groundwater to the surface"
+        },
+        ". Groundwater is also the source of water for springs. "
+      ]
+    },
+    {
+      "title": "Measure the groundwater model",
+      "start": 318.63,
+      "end": 357.66,
+      "timeLabel": "5:18–5:57",
+      "parts": [
+        "Well, for groundwater, we need to put 13 milliliters. So this time we need ",
+        {
+          "id": 16,
+          "answer": "more than just a dropper full"
+        },
+        ". I'm going to carefully measure out 13... milliliters. I'm going to get it just right because we're dealing with science. All right. Well, ",
+        {
+          "id": 17,
+          "answer": "it still doesn't look like very much water"
+        },
+        ". But you can see ",
+        {
+          "id": 18,
+          "answer": "there's more groundwater than there is water in lakes, rivers, and streams"
+        },
+        " or water in the atmosphere. And remember, all of these are fresh water. "
+      ]
+    },
+    {
+      "title": "Frozen freshwater",
+      "start": 359.33000000000004,
+      "end": 392.08,
+      "timeLabel": "5:59–6:32",
+      "parts": [
+        "Next, ",
+        {
+          "id": 19,
+          "answer": "we have ice caps and glaciers"
+        },
+        ". And notice that this is also fresh water. Look how much we need to put in the cup this time. 47 milliliters. This is a lot ",
+        {
+          "id": 20,
+          "answer": "more than we've put in any of the other freshwater cups"
+        },
+        ". And this is because ",
+        {
+          "id": 21,
+          "answer": "most of the freshwater on Earth is frozen as ice"
+        },
+        " near the north and south poles of the Earth. If we show this on a globe, most of the freshwater on Earth is frozen up near the north pole and near the south pole of the Earth. "
+      ]
+    },
+    {
+      "title": "Use math to measure",
+      "start": 394.66,
+      "end": 466.57,
+      "timeLabel": "6:34–7:46",
+      "parts": [
+        "So let's go ahead and measure these 47 milliliters. Here I have 30 milliliters. We need 47. Put in some more. Here I have 15 more, which makes 45. ",
+        {
+          "id": 22,
+          "answer": "We need two more milliliters"
+        },
+        ". You're dealing with amounts this small. It's kind of hard. Get it just right. There we go. Two more milliliters. Now we have 47. 47 milliliters. ",
+        {
+          "id": 23,
+          "answer": "Notice that I had to use some math there"
+        },
+        ". ",
+        {
+          "id": 24,
+          "answer": "We have to use math all the time in science"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Oceans and seas",
+      "start": 469.24,
+      "end": 511.76,
+      "timeLabel": "7:49–8:31",
+      "parts": [
+        "All right, so ",
+        {
+          "id": 25,
+          "answer": "last in our table, we have oceans and seas"
+        },
+        ". And this is salt water. Look how much needs to go in this cup. 2,139 milliliters. Now that is ",
+        {
+          "id": 26,
+          "answer": "a lot of water, especially compared to all these other amounts"
+        },
+        ". In fact, it's ",
+        {
+          "id": 27,
+          "answer": "all the water that's left here in this glass bowl"
+        },
+        ". So we could just label this oceans and seas. And it's salt water. We call it salt water because it has salt. So let's add some salt to this water. "
+      ]
+    },
+    {
+      "title": "Salt and valuable freshwater",
+      "start": 528.7900000000001,
+      "end": 579.64,
+      "timeLabel": "8:48–9:39",
+      "parts": [
+        "Well, where does this salt water in the oceans and seas get its salt? Well, some of its salt comes from rocks on the bottom of the ocean floor. And ",
+        {
+          "id": 28,
+          "answer": "other salt is washed off land"
+        },
+        " and into the ocean and seas. and ",
+        {
+          "id": 29,
+          "answer": "the salt dissolves in the ocean water"
+        },
+        ", making it salt water. Now this salt water, because of the salt that's in it, is different than fresh water. It can't be used for drinking, for watering plants, or washing clothes. It can't be used to make things in the factories. The fresh water, though there's not as much of it, especially when you compare it with the amount of salt water, this fresh water is very valuable on Earth. And we need to make sure that we ",
+        {
+          "id": 30,
+          "answer": "use it wisely and we don't pollute it"
+        },
+        ". So that's one way that we can model Earth's water. "
+      ]
+    }
+  ]
+};
