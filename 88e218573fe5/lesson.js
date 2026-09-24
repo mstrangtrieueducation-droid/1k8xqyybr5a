d54f70297654ef1eb0ff3495d52aae6fd5ea3991
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 19:06:00 +0700

    Add reviewed Science G3 Video 52 lesson

---
+++ 88e218573fe5/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-52",
+  "number": 52,
+  "title": "Reduce, Reuse and Recycle",
+  "duration": 413.7099375,
+  "durationLabel": "6 phút 53 giây",
+  "total": 30,
+  "audio": "88e218573fe5/audio.mp3",
+  "mindmap": "88e218573fe5/mindmap.png",
+  "questionsImage": "88e218573fe5/questions.jpg",
+  "speakingPoints": [
+    "Reduce: use less",
+    "Reuse: use again",
+    "Recycle: make new",
+    "Prepare the aluminum",
+    "Melt and reshape",
+    "Help the cycle continue"
+  ],
+  "questions": [
+    {
+      "text": "What does reduce mean? Use two examples from the video to explain how using less can conserve natural resources."
+    },
+    {
+      "text": "Choose two examples of reuse from the video. Explain how each keeps an object useful instead of throwing it away."
+    },
+    {
+      "text": "How is reusing a glass jar different from recycling an aluminum can? Explain what happens to the object or material in each case."
+    },
+    {
+      "text": "Describe the main steps in recycling aluminum shown in the video. What do the magnets remove, and what happens after the metal melts?"
+    },
+    {
+      "text": "How can a family support recycling? Include sorting items correctly and choosing useful recycled products, and explain why reducing and reusing still matter."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "The three Rs",
+      "start": 13.11,
+      "end": 46.65,
+      "timeLabel": "0:13–0:46",
+      "parts": [
+        "Today we are going to ",
+        {
+          "id": 1,
+          "answer": "learn about the three R's"
+        },
+        ". Reduce, reuse, and recycle. Now why are these three words important? Well they have to do with ",
+        {
+          "id": 2,
+          "answer": "protecting our natural resources"
+        },
+        " and taking care of them. Well let's take a look at this first one, reduce. What does that mean? Well reduce means to ",
+        {
+          "id": 3,
+          "answer": "bring down to a smaller amount"
+        },
+        ". size, extent, number, et cetera. So if we reduce something, it basically just means we make it smaller or we make it less. "
+      ]
+    },
+    {
+      "title": "Reduce waste",
+      "start": 47.2,
+      "end": 92.78,
+      "timeLabel": "0:47–1:32",
+      "parts": [
+        "Well, think about this in terms of our natural resources. If we reduce the amount of natural resources we use, ",
+        {
+          "id": 4,
+          "answer": "then we use less of them"
+        },
+        ". What are some ways that we could reduce the amount of resources that we use? We can reduce the amount of natural resources we use ",
+        {
+          "id": 5,
+          "answer": "by not wasting them"
+        },
+        ". So things like Not wasting water and not wasting energy can help to reduce these natural resources. Also, things like when we write on paper, ",
+        {
+          "id": 6,
+          "answer": "maybe writing on both sides"
+        },
+        " so we don't waste paper, there's many ways that we can reduce the amount of natural resources we use by just making wise choices and using less of them when we don't need to use more. "
+      ]
+    },
+    {
+      "title": "Use things again",
+      "start": 94.86999999999999,
+      "end": 124.64,
+      "timeLabel": "1:34–2:04",
+      "parts": [
+        "Let's look at this next word, reuse. What do you think that means? Well, when we reuse something, ",
+        {
+          "id": 7,
+          "answer": "that just means that we use it again"
+        },
+        ". So how could we reuse things? We can reuse many things instead of just throwing them away. So up here I have a few examples. We can ",
+        {
+          "id": 8,
+          "answer": "save buttons from old clothes"
+        },
+        " so we can reuse them. If we use cloth napkins, ",
+        {
+          "id": 9,
+          "answer": "we can wash them and reuse them"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Give objects another use",
+      "start": 125.64999999999999,
+      "end": 159.73000000000002,
+      "timeLabel": "2:05–2:39",
+      "parts": [
+        "And empty food containers can be ",
+        {
+          "id": 10,
+          "answer": "used to store different things"
+        },
+        " like money or food. or maybe even special science things that you want to study. So glass jars can be reused. And also ",
+        {
+          "id": 11,
+          "answer": "some shoes and toys"
+        },
+        " that you're too big for. You can give those away to other people or maybe sell them in a garage sale so other people can use them instead of just throwing them away. So there's many ways that we can reuse things. So we don't just throw them away. ",
+        {
+          "id": 12,
+          "answer": "We use them as much as we can"
+        },
+        " so we don't waste them. "
+      ]
+    },
+    {
+      "title": "What recycling means",
+      "start": 162.92,
+      "end": 201.12,
+      "timeLabel": "2:42–3:21",
+      "parts": [
+        "And the last R-word Recycle. Here's an interesting one. Recycle means to treat or process used or waste materials. So used or waste materials are basically just trash. So as to ",
+        {
+          "id": 13,
+          "answer": "make suitable to use again"
+        },
+        ". So basically we take our garbage and if it's a recyclable kind of garbage that can be recycled, then people can treat it and ",
+        {
+          "id": 14,
+          "answer": "put it through a process"
+        },
+        " so that it can be used to make something else, to make something new. Let's take a look at ",
+        {
+          "id": 15,
+          "answer": "how aluminum can be recycled"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Shred, sort and remove paint",
+      "start": 204.41,
+      "end": 256.81,
+      "timeLabel": "3:24–4:16",
+      "parts": [
+        "So the aluminum is ",
+        {
+          "id": 16,
+          "answer": "shredded into tiny pieces"
+        },
+        " and then sorted by magnets. Here you can see tiny shreds of aluminum and then they use magnets to pull out any pieces of steel that might have gotten ",
+        {
+          "id": 17,
+          "answer": "mixed in accidentally"
+        },
+        " because they want to make sure that this is just aluminum that they're recycling. They recycle steel separately. Next, any paint or lacquer is removed by ",
+        {
+          "id": 18,
+          "answer": "blowing very hot air on the shreds"
+        },
+        ". So you can see that these shreds have a lot of color on them. But when they do this step of removing that paint, all those labels will disappear. They'll blow this hot air on them to get rid of all of this paint that's left on the aluminum. And then we're ready for the next step. "
+      ]
+    },
+    {
+      "title": "Melt and cast",
+      "start": 257.1,
+      "end": 277.53000000000003,
+      "timeLabel": "4:17–4:37",
+      "parts": [
+        "The aluminum is then melted, ",
+        {
+          "id": 19,
+          "answer": "treated to remove impurities, and recast"
+        },
+        ". So here you can see that this melted aluminum, ",
+        {
+          "id": 20,
+          "answer": "it's so hot that it's actually liquid"
+        },
+        ", is melted and treated to remove impurities. And then when it's recast, that means it's ",
+        {
+          "id": 21,
+          "answer": "poured into a mold and then cooled"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "New products from aluminum",
+      "start": 279.16,
+      "end": 328.87,
+      "timeLabel": "4:39–5:28",
+      "parts": [
+        "And finally, recycled aluminum can be taken to a place ",
+        {
+          "id": 22,
+          "answer": "where new things can be made"
+        },
+        ". So some of this recycled aluminum might be rolled out into very, very flat pieces and ",
+        {
+          "id": 23,
+          "answer": "made into new soda cans"
+        },
+        ", or it might be made into other things, but this process can begin again. The soda cans will be used, and then they can be recycled, and we can keep using up this aluminum. Now interestingly, ",
+        {
+          "id": 24,
+          "answer": "It takes less work to just recycle aluminum"
+        },
+        " than it takes to mine it out of the ground, purify it from the ores, and then use it to make something. So we can actually save some work and also really save and help protect this natural resource of aluminum when we recycle. "
+      ]
+    },
+    {
+      "title": "Buy recycled products",
+      "start": 331.56,
+      "end": 359.66,
+      "timeLabel": "5:31–5:59",
+      "parts": [
+        "There are many things that we can buy that are made out of recycled materials. Let's look at a couple examples. ",
+        {
+          "id": 25,
+          "answer": "There's recycled paper"
+        },
+        ". Often when you're at the store, you can find some paper that will say on it that it's recycled. And they even make ",
+        {
+          "id": 26,
+          "answer": "benches made out of recycled material"
+        },
+        ". And ",
+        {
+          "id": 27,
+          "answer": "even sweaters can be made"
+        },
+        " out of recycled materials. So there's many different things that can be made from materials that have been recycled. "
+      ]
+    },
+    {
+      "title": "Help recycling work",
+      "start": 361.71000000000004,
+      "end": 410.57,
+      "timeLabel": "6:01–6:50",
+      "parts": [
+        "We can help support recycling. Now what are some ways that we can help support recycling? Well, one way is to put our drink cans, our glass and plastic bottles, and our paper ",
+        {
+          "id": 28,
+          "answer": "in the right recycling bins"
+        },
+        ". Sometimes in public places, they have recycling bins, and if we put our garbage in the right ones, then they can be recycled. And also, ",
+        {
+          "id": 29,
+          "answer": "we can sort our garbage in our homes"
+        },
+        " in different things that can be recycled, like glass, paper, and the metal, and then we can take it to the different places where our city will recycle things. So today we've learned about the three R's. Reduce, reuse, and recycle. And by doing these three things, we can ",
+        {
+          "id": 30,
+          "answer": "help to conserve our natural resources"
+        },
+        ". "
+      ]
+    }
+  ]
+};
