270bb0fd68d08e301640cf69909e0509dc1a6f21
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 19:39:07 +0700

    Add reviewed Science G3 Video 54 lesson

---
+++ 88ceef15da85/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-54",
+  "number": 54,
+  "title": "Modeling Erosion",
+  "duration": 516.66725,
+  "durationLabel": "8 phút 36 giây",
+  "total": 30,
+  "audio": "88ceef15da85/audio.mp3",
+  "mindmap": "88ceef15da85/mindmap.png",
+  "questionsImage": "88ceef15da85/questions.jpg",
+  "speakingPoints": [
+    "Erosion means movement",
+    "Wind moves dry sand",
+    "Mud and rocks in wind",
+    "A moving-water test",
+    "Compare visible changes",
+    "Explain the evidence"
+  ],
+  "questions": [
+    {
+      "text": "What is erosion? Explain how the video models erosion and how erosion differs from material simply breaking down in place."
+    },
+    {
+      "text": "What happened when the teacher blew on the sand, mud, and rocks? Include the measured distance and explain one reason the materials behaved differently."
+    },
+    {
+      "text": "How did the rain cup and tilted tray model moving water? Why was the same amount of water used over each pile?"
+    },
+    {
+      "text": "Compare the three piles after the water test. What changes in the sand and mud show that erosion happened?"
+    },
+    {
+      "text": "The rocks did not move in this model. Does that mean flowing water can never move rocks? Explain using the video's conclusion and the limits of this small test."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "Make an erosion model",
+      "start": 12.9,
+      "end": 62.54,
+      "timeLabel": "0:12–1:02",
+      "parts": [
+        "Today we're going to be doing an activity where we model erosion on different substances. Now do you remember what erosion is? It's ",
+        {
+          "id": 1,
+          "answer": "when weathered material is moved"
+        },
+        ". And so here in this container I have ",
+        {
+          "id": 2,
+          "answer": "a pile of sand, a pile of mud"
+        },
+        ", and a pile of rocks. And we're going to be looking at erosion by wind and erosion by water on these different substances. And then we're going to ",
+        {
+          "id": 3,
+          "answer": "record what happens in a table"
+        },
+        ". All right, so let's get started. Now the first thing we're going to model is erosion by wind. And to do this, I'm just going to blow on each of these piles. But first I need to put on safety goggles, because when we blow on things, some of the particles can blow into our eyes, which can hurt our eyes. And so the safety goggles are for protection. "
+      ]
+    },
+    {
+      "title": "Blow and measure the sand",
+      "start": 64.82,
+      "end": 123.86000000000001,
+      "timeLabel": "1:04–2:03",
+      "parts": [
+        "All right, first I'm going to blow on the sand, and I want you to ",
+        {
+          "id": 4,
+          "answer": "watch closely at what happens"
+        },
+        ". See if we can see any erosion. Did any of the sand move? Now let's see what happens when I blow on the mud. Did anything happen this time? Let's try blowing on the rocks. Did anything happen? Now what were we able to observe? When I blew on the sand, ",
+        {
+          "id": 5,
+          "answer": "the sand moved very far"
+        },
+        ". Let's measure how far it moved. It actually moved all the way into this farthest corner. So if I use my ruler to measure it, ",
+        {
+          "id": 6,
+          "answer": "It moved about 12 inches"
+        },
+        ". So the sand really moved. It really eroded in the wind. "
+      ]
+    },
+    {
+      "title": "Notice mud and rocks",
+      "start": 124.19,
+      "end": 150.82,
+      "timeLabel": "2:04–2:30",
+      "parts": [
+        "What about the mud? Well, the mud didn't really have any effect. It just sat there. And remember, erosion by wind is especially common in places where it's dry, ",
+        {
+          "id": 7,
+          "answer": "where it's not very wet"
+        },
+        ". ",
+        {
+          "id": 8,
+          "answer": "Mud is wet and sticky"
+        },
+        ". And erosion by wind didn't have as much effect here. And the rocks didn't erode much by wind either. So let's ",
+        {
+          "id": 9,
+          "answer": "record what we've seen in our table"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "Record the wind results",
+      "start": 152.09,
+      "end": 198.45000000000002,
+      "timeLabel": "2:32–3:18",
+      "parts": [
+        "So in our table, you can see this table shows erosion by wind and water, and we're going to fill in this column, effect of blowing, ",
+        {
+          "id": 10,
+          "answer": "which is modeling the wind"
+        },
+        ". So the sand, when we blew on the sand, it moved 12 inches. So I'm going to write moved 12 inches. basically moved as far as it could in our container. ",
+        {
+          "id": 11,
+          "answer": "It might have moved farther"
+        },
+        " if our container was bigger. And then the mud didn't move. It just kind of sat there. We'll write, didn't move. And ",
+        {
+          "id": 12,
+          "answer": "the rocks didn't move either"
+        },
+        ". So again, we'll write, didn't move. "
+      ]
+    },
+    {
+      "title": "Model moving water",
+      "start": 204.41,
+      "end": 234.74,
+      "timeLabel": "3:24–3:54",
+      "parts": [
+        "Now we need to ",
+        {
+          "id": 13,
+          "answer": "look at the effect of moving water"
+        },
+        ". Now to model the erosion by water, ",
+        {
+          "id": 14,
+          "answer": "we're going to use a rain cup"
+        },
+        ". And notice that in this cup, I've poked four large holes. So what we're going to do is ",
+        {
+          "id": 15,
+          "answer": "pour the same amount of water into the cup each time"
+        },
+        " over the sand, over the mud, and over the rocks. And we're going to observe what happens, how much erosion will take place. But first let's prop up this with an eraser. "
+      ]
+    },
+    {
+      "title": "Pour water over each pile",
+      "start": 238.0,
+      "end": 289.11,
+      "timeLabel": "3:58–4:49",
+      "parts": [
+        "All right, first we'll pour water on the sand. So I'm going to get my water and each time we're going to use ",
+        {
+          "id": 16,
+          "answer": "about 125 milliliters"
+        },
+        ". And I'll pour it over. the sand. Was any of the sand moving? ",
+        {
+          "id": 17,
+          "answer": "Is there any erosion taking place"
+        },
+        "? Now let's do the same thing, pouring over the mud. I'm pouring the same amount of water. What do you observe? Did anything happen? Now let's do the same thing, ",
+        {
+          "id": 18,
+          "answer": "pouring water over the rocks"
+        },
+        ". Is anything happening to the rocks? "
+      ]
+    },
+    {
+      "title": "Observe the water results",
+      "start": 292.75,
+      "end": 349.91,
+      "timeLabel": "4:52–5:49",
+      "parts": [
+        "So what were we able to observe this time? Well, when we poured the water over the sand, ",
+        {
+          "id": 19,
+          "answer": "There was a lot of movement"
+        },
+        ". In fact, the sand moved again as far as it could in the pan. And if we measure it, again, this is about 12 inches. What about the mud? When we poured the water over the mud, again, it moved. And it moved with that water as far as it could. Now, you can see that ",
+        {
+          "id": 20,
+          "answer": "the pile of mud is still more like it was to begin with"
+        },
+        " than the sand. But the mud has also eroded. You can see where ",
+        {
+          "id": 21,
+          "answer": "the mud has washed down and away"
+        },
+        " in the water. And then if we look at the rocks, the rocks didn't really have any change. They just stayed how they were. So let's record what we've learned in our table. "
+      ]
+    },
+    {
+      "title": "How the sand changed",
+      "start": 351.59000000000003,
+      "end": 382.99,
+      "timeLabel": "5:51–6:22",
+      "parts": [
+        "So let's ",
+        {
+          "id": 22,
+          "answer": "fill in this column"
+        },
+        ", Effect of Moving Water. Now when we poured the moving water on the sand, what happened? Well, a lot of the sand eroded. In fact, that pile of sand that used to be kind of tall ",
+        {
+          "id": 23,
+          "answer": "got much more flat"
+        },
+        " ",
+        {
+          "id": 24,
+          "answer": "because that sand washed away"
+        },
+        ". And so I think we should put here, eroded the most. Don't you agree that the sand eroded the most? Eroded most. "
+      ]
+    },
+    {
+      "title": "How mud and rocks changed",
+      "start": 386.36,
+      "end": 435.03000000000003,
+      "timeLabel": "6:26–7:15",
+      "parts": [
+        "And then when we poured the moving water over the mud, what happened? It eroded and that mud went with the water and moved. to a different place. But there was still more mud than there was sand. The mud was still pretty much in the same sort of pile, but ",
+        {
+          "id": 25,
+          "answer": "its shape changed"
+        },
+        " because some of that mud was moved away. So we're going to say that the mud eroded some. Because it did erode. It did move. But ",
+        {
+          "id": 26,
+          "answer": "it didn't quite move as much as the sand"
+        },
+        ". All right, eroded. And then what about the rocks? Did the rocks erode from this moving water? No, ",
+        {
+          "id": 27,
+          "answer": "not with this moving water"
+        },
+        ". So we'll just say the rocks didn't move. "
+      ]
+    },
+    {
+      "title": "Explain this model",
+      "start": 444.64000000000004,
+      "end": 513.62,
+      "timeLabel": "7:24–8:33",
+      "parts": [
+        "So what were we able to learn from this experiment? Well, one thing we learned is that the effect of blowing or the erosion by wind happened on the sand the most. ",
+        {
+          "id": 28,
+          "answer": "The dry sand could easily blow away"
+        },
+        " with the wind, whereas the wet mud didn't move, and those huge rocks didn't move either. And in the water, The sand and the mud eroded. But those rocks didn't move. Now I want you to notice something. The sand is very, very weathered material. The sand is made of ",
+        {
+          "id": 29,
+          "answer": "rocks that were broken into really, really tiny pieces"
+        },
+        ". And the mud also has tiny pieces of rock in it. But the rocks were quite large still. And so they didn't erode quite as easily. They weren't quite as weathered. And so... Rocks would take a stronger gust of wind or ",
+        {
+          "id": 30,
+          "answer": "a stronger current of water"
+        },
+        " to get them to move than the sand. So these are some things that we can learn by observing erosion. "
+      ]
+    }
+  ]
+};
