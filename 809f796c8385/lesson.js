fdd2aadb1f0e6055b9dd5cbe1e25e511760049b0
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-24 14:35:42 +0700

    Add reviewed Science G3 Video 25 lesson

---
+++ 809f796c8385/lesson.js
@@ -0,0 +1,278 @@
+window.SCIENCE_LESSON = {
+  "id": "science-g3-25",
+  "number": 25,
+  "title": "Mixtures",
+  "duration": 371.456,
+  "durationLabel": "6 phút 11 giây",
+  "total": 30,
+  "audio": "809f796c8385/audio.mp3",
+  "mindmap": "809f796c8385/mindmap.png",
+  "questionsImage": "809f796c8385/questions.jpg",
+  "speakingPoints": [
+    "Two or more kinds",
+    "No new substance",
+    "Choose a method",
+    "Size makes it work",
+    "Salad",
+    "Trail mix"
+  ],
+  "questions": [
+    {
+      "text": "What is a mixture? Must it contain equal amounts of each kind of matter? Use the marbles and sand to explain."
+    },
+    {
+      "text": "How do we know the marbles and sand have not become a new substance? What stays the same after mixing?"
+    },
+    {
+      "text": "Describe two ways to separate marbles from sand. Why can a strainer work faster than picking out the marbles by hand?"
+    },
+    {
+      "text": "Why is a salad a mixture? Explain what happens to the vegetables when they are mixed and how you could separate them."
+    },
+    {
+      "text": "Why is trail mix a mixture? Use it to explain the three important properties of mixtures described in the video."
+    }
+  ],
+  "chunks": [
+    {
+      "title": "What is a mixture?",
+      "start": 7.39,
+      "end": 35.54,
+      "timeLabel": "0:07–0:35",
+      "parts": [
+        "Today we're going to be talking about mixtures. Now we use mixtures ",
+        {
+          "id": 1,
+          "answer": "all the time in everyday life"
+        },
+        " and also in science. And in science, mixtures has ",
+        {
+          "id": 2,
+          "answer": "a certain definition"
+        },
+        ". A mixture is made when ",
+        {
+          "id": 3,
+          "answer": "two or more kinds of matter are placed together"
+        },
+        ". So that makes sense. We can make a mixture when we place two or more kinds of matter together. Well, let's make a mixture today. "
+      ]
+    },
+    {
+      "title": "Marbles and sand",
+      "start": 36.440000000000005,
+      "end": 72.81,
+      "timeLabel": "0:36–1:12",
+      "parts": [
+        "So here I have ",
+        {
+          "id": 4,
+          "answer": "two different types of matter"
+        },
+        ". I have some marbles and I have some sand. Now if we mix these two types of matter together, we can make a mixture. Now notice that there isn't the same amount of each type of matter here. We have ",
+        {
+          "id": 5,
+          "answer": "more marbles than sand"
+        },
+        ". Well, that's fine. ",
+        {
+          "id": 6,
+          "answer": "There can be different amounts"
+        },
+        " of matter in mixtures, but the thing that makes it a mixture is when we mix two or more kinds of matter together. and we put them together. So let's make a mixture as we put the marbles and the sand together. "
+      ]
+    },
+    {
+      "title": "Look for a new substance",
+      "start": 78.24,
+      "end": 108.10000000000001,
+      "timeLabel": "1:18–1:48",
+      "parts": [
+        "So here is our mixture. Now I want you to ",
+        {
+          "id": 7,
+          "answer": "look closely at this"
+        },
+        ". Do you see any ",
+        {
+          "id": 8,
+          "answer": "new type of substance forming"
+        },
+        "? Are there some gas bubbles of a new type of gas forming? Maybe carbon dioxide? No. Is there a new type of solid here? No. It's ",
+        {
+          "id": 9,
+          "answer": "still just marbles and sand"
+        },
+        ". "
+      ]
+    },
+    {
+      "title": "The parts stay the same",
+      "start": 109.22999999999999,
+      "end": 148.24,
+      "timeLabel": "1:49–2:28",
+      "parts": [
+        "Well, this is actually a very important thing about mixtures. It's a property about mixtures in science. Each kind of matter in a mixture ",
+        {
+          "id": 10,
+          "answer": "does not change into another substance"
+        },
+        ". So in our mixture here, the marbles and the sand, they're not changing into a different substance. They're just staying the same. ",
+        {
+          "id": 11,
+          "answer": "The marbles are still just marbles"
+        },
+        ". ",
+        {
+          "id": 12,
+          "answer": "The sand is still just sand"
+        },
+        ". We have the same substances and they're not changing. They're just mixed together. "
+      ]
+    },
+    {
+      "title": "How could we separate them?",
+      "start": 149.38,
+      "end": 184.56,
+      "timeLabel": "2:29–3:04",
+      "parts": [
+        "Now here's another really important thing about mixtures. Each kind of matter in a mixture ",
+        {
+          "id": 13,
+          "answer": "can be separated apart"
+        },
+        ". So can you think of how we could separate this mixture apart? How could we separate the marbles from the sand? Well, one way is we could pick out all the marbles. But ",
+        {
+          "id": 14,
+          "answer": "that might take a long time"
+        },
+        ". Can you think of another way we could separate this mixture? Sometimes we can use special things we know about ",
+        {
+          "id": 15,
+          "answer": "the properties of the matter"
+        },
+        " to separate them. Some mixtures can be separated quite easily. "
+      ]
+    },
+    {
+      "title": "Choosing a colander",
+      "start": 186.42,
+      "end": 212.52,
+      "timeLabel": "3:06–3:32",
+      "parts": [
+        "Well, maybe ",
+        {
+          "id": 16,
+          "answer": "we could use a colander"
+        },
+        ". How could a colander help us to separate this mixture? Well, notice that the two different types of matter here are ",
+        {
+          "id": 17,
+          "answer": "two different sizes"
+        },
+        ". What will happen if we ",
+        {
+          "id": 18,
+          "answer": "pour this mixture into the colander"
+        },
+        "? Will it help to separate our mixture? Well, let's take a look. So here I have my mixture and I'll pour it into the colander. "
+      ]
+    },
+    {
+      "title": "Why the strainer works",
+      "start": 219.37,
+      "end": 244.98000000000002,
+      "timeLabel": "3:39–4:04",
+      "parts": [
+        "Notice that ",
+        {
+          "id": 19,
+          "answer": "the mixture has been separated"
+        },
+        ". Here I have the marbles and in the bowl is the sand. While the sand was so small, it was able to go ",
+        {
+          "id": 20,
+          "answer": "right through the holes"
+        },
+        " in the colander. but ",
+        {
+          "id": 21,
+          "answer": "the marbles were too large"
+        },
+        " and they were left behind. So this was a very easy mixture to separate using a strainer. "
+      ]
+    },
+    {
+      "title": "A salad is a mixture",
+      "start": 246.73999999999998,
+      "end": 295.86,
+      "timeLabel": "4:06–4:55",
+      "parts": [
+        "Now, I want you to think of a mixture that you used or saw today. Can you think of one? Well, what about a salad? A salad is actually a mixture. It's a mixture of ",
+        {
+          "id": 22,
+          "answer": "many different kinds of vegetables"
+        },
+        " that have been put together. Well, are these vegetables changing into a new type of matter, changing into a new substance because they're mixed together? No. ",
+        {
+          "id": 23,
+          "answer": "The tomatoes are still tomatoes"
+        },
+        ". ",
+        {
+          "id": 24,
+          "answer": "The lettuce is still lettuce"
+        },
+        ". Each vegetable is the same type of matter as it was before it was put into the mixture. And if we wanted to, we could even separate out each type of matter just by sorting out each different type of vegetable. So a salad is a mixture. Can you think of another one? "
+      ]
+    },
+    {
+      "title": "Trail mix",
+      "start": 297.46000000000004,
+      "end": 324.15000000000003,
+      "timeLabel": "4:57–5:24",
+      "parts": [
+        "Well, what about a trail mix? A trail mix is a mixture of ",
+        {
+          "id": 25,
+          "answer": "many types of nuts and fruit"
+        },
+        ". And again, the nuts and fruit that are put inside the trail mix aren't being changed into ",
+        {
+          "id": 26,
+          "answer": "a different type of substance"
+        },
+        ". They're just the same as they were before they were mixed together. And we could very easily separate ",
+        {
+          "id": 27,
+          "answer": "each type of fruit and nut"
+        },
+        " from the mixture. So this is another type of mixture. "
+      ]
+    },
+    {
+      "title": "Three key properties",
+      "start": 325.08000000000004,
+      "end": 362.63,
+      "timeLabel": "5:25–6:02",
+      "parts": [
+        "So ",
+        {
+          "id": 28,
+          "answer": "mixtures are all around us"
+        },
+        ", but there's some important things that I want you to remember about mixtures. Mixtures are made of two or more kinds of matter. The matter inside mixtures ",
+        {
+          "id": 29,
+          "answer": "does not change into a new substance"
+        },
+        ". It stays the same as when you first mixed it together. The different kinds of matter in a mixture can be separated apart. So if you want to, you can separate apart ",
+        {
+          "id": 30,
+          "answer": "the different types of matter in the mixture"
+        },
+        ". So today we've learned about mixtures. "
+      ]
+    }
+  ]
+};
